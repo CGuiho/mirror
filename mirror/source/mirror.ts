10407bf50a3080d1cb7ad5da8033e24664bcff91
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2025-03-15 17:06:59 +0100

    Enhance mirror.ts to support help message and improve argument handling for version bumping

--- mirror/source/mirror.ts
+++ mirror/source/mirror.ts
@@ -1,18 +1,27 @@
 #!/usr/bin/env bun
-
 import { $ } from 'bun'
 import { fail } from 'node:assert'
 import { type ReleaseType, inc, valid as isValidVersion } from 'semver'
 
-const projectName = process.argv[2]
-const pathPackageJson = process.argv[3]
-const targetVersion = Bun.argv[4]
+const usage = `Usage: ${process.argv[1]} <path-to-package.json> <semver-valid-target-version> \n -h, --help: Show this message`
 
-const usage = `Usage: ${process.argv[1]} <project-name> <path-to-package.json> <semver-valid-target-version>`
+if (process.argv[2] === '--help' || process.argv[2] === '-h') {
+  console.log(usage)
+  process.exit(0)
+}
 
-if (!projectName) throw new Error('No project name provided. \n' + usage)
-if (!pathPackageJson) throw new Error('No path provided. \n' + usage)
-if (!targetVersion) throw new Error('No target version provided. \n' + usage)
+let pathPackageJson = './package.json'
+let targetVersion = ''
+
+if (process.argv[2] && process.argv[3]) {
+  pathPackageJson = process.argv[2]
+  targetVersion = process.argv[3]
+} else if (process.argv[2]) {
+  targetVersion = process.argv[2]
+} else {
+  console.error(usage)
+  process.exit(1)
+}
 
 const variants: ReleaseType[] = ['major', 'premajor', 'minor', 'preminor', 'patch', 'prepatch', 'prerelease']
 
@@ -20,12 +29,12 @@ const isValidTarget = (subject: string): subject is ReleaseType => (variants as 
 
 const isDirty = async () => (await $`git status --porcelain`.quiet()).text()
 
-const json = await Bun.file(pathPackageJson).json()
-const { version: current } = json
+const json = await Bun.file(pathPackageJson || './package.json').json()
+const { version: current, name } = json
 
 if (!isValidVersion(current)) throw new Error(`Invalid current version ${current}`)
 
-if (await isDirty()) console.warn('There are uncommitted changes. Commit them before releasing.')
+if (await isDirty()) console.warn('🚧😬 There are uncommitted changes. Commit them before releasing.')
 
 const desired = isValidVersion(targetVersion)
   ? targetVersion
@@ -39,7 +48,10 @@ console.debug(current, '—>', desired)
 await Bun.write(pathPackageJson, JSON.stringify(Object.assign(json, { version: desired }), null, 2))
 
 await $`git add ${pathPackageJson}`
-await $`git commit -m ${projectName}@${desired}`
-await $`git tag ${projectName}@${desired} -m "Release ${projectName}@${desired}"`
+await $`git commit -m ${name}@${desired}`
+await $`git tag ${name}@${desired} -m "Release ${name}@${desired}"`
 await $`git push`
 await $`git push --tags`
+
+console.info('✅ Done')
+console.info('🚀🎉 GUIHO Mirror')
