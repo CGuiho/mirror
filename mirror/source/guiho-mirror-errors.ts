1af8816b549bbe40b6afb926a997aac58192b6cf
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-05-16 10:33:04 +0200

    feat: add guiho-mirror-errors.ts

---
+++ mirror/source/guiho-mirror-errors.ts
@@ -0,0 +1,13 @@
+export class MirrorError extends Error {
+  readonly exitCode: number
+
+  constructor(message: string, exitCode = 1) {
+    super(message)
+    this.name = 'MirrorError'
+    this.exitCode = exitCode
+  }
+}
+
+export const invariant = (condition: unknown, message: string): asserts condition => {
+  if (!condition) throw new MirrorError(message)
+}
