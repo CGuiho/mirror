ee24f8a0c28a1c5b346505117e447bc867e11454
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-05-16 10:59:53 +0200

    copyright: add header to reporter.ts

--- mirror/source/reporter.ts
+++ mirror/source/reporter.ts
@@ -1,3 +1,7 @@
+/**
+ * @copyright Copyright (c) 2026 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved.
+ */
+
 import type { MirrorConfig, MirrorExecutionResult, MirrorFormat, MirrorVersionPlan } from './types'
 import { configPathForDisplay, relativeFromCwd } from './config'
 
