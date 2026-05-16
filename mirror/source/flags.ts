91a1875c961098b037e489294281253996071fdb
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-05-16 10:59:52 +0200

    copyright: add header to flags.ts

--- mirror/source/flags.ts
+++ mirror/source/flags.ts
@@ -1,3 +1,7 @@
+/**
+ * @copyright Copyright (c) 2026 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved.
+ */
+
 import type { MirrorAdapterName, MirrorCliOptions, MirrorFormat } from './types'
 import { MirrorError } from './errors'
 
