ec53339179451733ba081fb340dceb1f86ab89a2
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-07-23 20:05:09 +0200

    Export Mirror download progress type

--- mirror/source/guiho-mirror.ts
+++ mirror/source/guiho-mirror.ts
@@ -25,6 +25,7 @@ export type {
   MirrorAvailableRelease,
   MirrorReleaseCatalog,
   MirrorUpgradeEvent,
+  MirrorUpgradeDownloadProgress,
   MirrorUpgradeFailure,
   MirrorUpgradeFailureCode,
   MirrorUpgradeOutcome,
