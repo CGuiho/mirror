78d6207651654aa2b06acde52997d59227f9093a
Cristóvão GUIHO <cristovaoguiho@gmail.com>
2026-07-24 01:33:10 +0200

    feat(go): add top-level main.go entry point

---
+++ main.go
@@ -0,0 +1,11 @@
+/**
+ * @copyright Copyright © 2026 GUIHO Technologies as represented by Cristóvão GUIHO. All Rights Reserved.
+ */
+
+package main
+
+import "github.com/CGuiho/mirror/cmd"
+
+func main() {
+	cmd.Execute()
+}
